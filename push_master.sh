9ad30eef8520a00cf89d298d8c3731f60b86fce7
edward <gyurse@gmail.com>
2020-05-03 02:26:55 +0900

    20200503 : OSX

--- push_master.sh
+++ push_master.sh
@@ -1,3 +1,19 @@
 git add . -A
-git commit -m "from linux : $(lsb_release -sd)" 
+
+OS=$(uname)
+if [[ "$OS" == "Darwin" ]]; then
+	if [ -z "$1" ]
+	then
+		git commit -m "$(date +%Y%m%d) : OSX" 
+	else
+		git commit -m "$1" 
+	fi
+else
+	if [ -z "$1" ]
+	then
+		git commit -m "$(date +%Y%m%d) : $(lsb_release -sd)" 
+	else
+		git commit -m "$1" 
+	fi
+fi
 git push origin master
